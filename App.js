23eca445c399c4368f1a1bf6bafe8a4bed6976ea
philipp <philipp.reiter90@gmail.com>
2023-08-08 15:14:13 +0200

    changed color so it matches uploaded videos

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
